525de32d8c6abd8657ca9ca67c53992432ef2157
emhar <emmanuel.harleaux@gmail.com>
2016-07-06 09:42:12 +0200

    Fix a bug in header generator

--- lib/security/WsseHeaderGenerator.php
+++ lib/security/WsseHeaderGenerator.php
@@ -11,10 +11,8 @@ class WsseHeaderGenerator
     public static function generateHeader($username, $secret){
         $nonce = sha1(openssl_random_pseudo_bytes(128));
         $created = date_create('NOW')->format(DateTime::ISO8601);
-
-
-        $provider = new WsseProvider(null);
-        $digest = $provider->generateDigest($nonce, $created, $secret);
+        
+        $digest = base64_encode(sha1(base64_decode($nonce) . $created . $secret, true));
 
         return sprintf(
             'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"' . "\n",
